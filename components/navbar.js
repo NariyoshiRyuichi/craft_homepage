1db05b400e8d0cd92e9d31aaea647725c17b0563
Ryuichi Nariyoshi <nariyoshiryuichi@gmail.com>
2022-01-07 14:28:37 +0900

    seventh commit

--- components/navbar.js
+++ components/navbar.js
@@ -22,10 +22,10 @@ const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue("gray200","whiteAlpha.900")
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref>
       <Link
 	p={2}
-	bg={active ? "glassTeal" : undefined}
+	bg={active ? "grassTeal" : undefined}
 	color={active ? "#202023" : inactiveColor}
 	_target={_target}
 	{...props}
