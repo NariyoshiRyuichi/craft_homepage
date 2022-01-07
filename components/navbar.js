d4309d96b07ee057fdb430eff44fad0b71e26119
Ryuichi Nariyoshi <nariyoshiryuichi@gmail.com>
2022-01-07 13:20:10 +0900

    sixth commit

--- components/navbar.js
+++ components/navbar.js
@@ -16,8 +16,9 @@ import {
 } from "@chakra-ui/react"
 import { HamburgerIcon } from "@chakra-ui/icons"
 import ThemeToggleButton from "./theme-toggle-button"
+import { IoLogoGithub } from 'react-icons/io5'
 
-const LinkItem = ({ href, path, children}) => {
+const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue("gray200","whiteAlpha.900")
   return (
@@ -26,6 +27,8 @@ const LinkItem = ({ href, path, children}) => {
 	p={2}
 	bg={active ? "glassTeal" : undefined}
 	color={active ? "#202023" : inactiveColor}
+	_target={_target}
+	{...props}
       >
        {children}
       </Link>
@@ -42,7 +45,7 @@ const Navbar = props => {
     as="nav"
     w="100%"
     bg={useColorModeValue("#ffffff40","#20202380")}
-    style={{backdropFilter:"blur(10px"}}
+    style={{backdropFilter:"blur(10px)"}}
     zIndex={1}
     {...props}
     >
@@ -55,13 +58,14 @@ const Navbar = props => {
 	justify="space-between"
       >
 	<Flex align="center" mr={5}>
-	 <Heading as="h1" size="lg"letterSpacint={'tighter'}>
+	 <Heading as="h1" size="lg" letterSpacint={'tighter'}>
 	  <Logo />
 	 </Heading>
 	</Flex>
 	<Stack
 	  direction={{base:"column",md:"row"}}
 	  display={{base:"none",md:"flex"}}
+	  width={{base:"full",md:"auto"}}
 	  alignItems="center"
 	  flexGrow={1}
 	  mt={{base:4,nmd:0}}
@@ -72,6 +76,18 @@ const Navbar = props => {
          <LinkItem href="/posts" path={path}>
            Posts
          </LinkItem>
+	 <LinkItem
+	  _target="_blank"
+	  href="https://github.com/NariyoshiRyuichi/craft_homepage"
+	  path={path}
+	  display="inline-flex"
+	  alignItems="center"
+	  style={{ gap: 4 }}
+	  pl={2}
+	 >
+	   <IoLogoGithub />
+	   Source
+	 </LinkItem>
 	</Stack>
 	<Box flex={1} align="right">
 	 <ThemeToggleButton />
